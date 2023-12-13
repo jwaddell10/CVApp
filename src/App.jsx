3da6b5db682c87e053853414e6b743e48cefe344
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2023-12-13 12:57:12 -0600

    change form on button click

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,60 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+const [activeIndex, setActiveIndex] = useState(0)
+
+const handleButtonClick = (index) => {
+  setActiveIndex(index);
+};
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  <div>
+    <div>
+      <h1>CV Application</h1>
+    </div>
+    <div>
+      <button onClick = {() => handleButtonClick(0)}>Personal Details:</button>
+      <button onClick = {() => handleButtonClick(1)}>Educational Experience:</button>
+      <button onClick = {() => handleButtonClick(2)}>Work Experience:</button>
+    </div>
+
+    {activeIndex === 0 && (
+        <form>
+          <h3 className = "formZeroInformation">Name:</h3>
+          <input type = "text" label = "name"></input>
+          <h3 className='formZeroInformation'>Email:</h3>
+          <input type = "email" label = "email"></input>
+          <h3 className='formZeroInformation'>Phone Number:</h3>
+          <input type = "tel" label = "telephone"></input>
+        </form>
+      )}
+    {activeIndex === 1 && (
+        <form>
+          <h3 className = "formOneInformation">School Name:</h3>
+          <input type = "text" label = "schoolName"></input>
+          <h3 className = "formOneInformation">Major:</h3>
+          <input type = "text" label = "studyProgram"></input>
+          <h3 className='formOneInformation'>Graduation Year:</h3>
+          <input type="number" label = "studyDates"/>
+        </form>
+      )}
+    {activeIndex === 2 && (
+        <form>
+          <h3 className = "formTwoInformation">Company Name:</h3>
+          <input type = "text" label = "schoolName"></input>
+          <h3 className = "formTwoInformation">Position:</h3>
+          <input type = "text" label = "studyProgram"></input>
+          <h3 className='formTwoInformation'>Start Date:</h3>
+          <input type="date" label = "startDate"/>
+          <h3 className='formTwoInformation'>End Date:</h3>
+          <input type="date" label = "endDate"/>
+        </form>
+      )}
+  </div>
+  ) 
 }
+
+
+//three buttons, when one is clicked display that form.
 
 export default App
